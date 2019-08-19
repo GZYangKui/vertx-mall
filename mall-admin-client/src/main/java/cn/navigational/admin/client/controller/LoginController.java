19ec2123ad660f71a5222c4fffd223329d1e97dd
navigatorCode <aaa123456>
2019-08-19 15:16:17 +0800

    实现窗口拖动功能

--- mall-admin-client/src/main/java/cn/navigational/admin/client/controller/LoginController.java
+++ mall-admin-client/src/main/java/cn/navigational/admin/client/controller/LoginController.java
@@ -1,19 +1,24 @@
 package cn.navigational.admin.client.controller;
 
+import cn.navigational.admin.client.monitors.WindowDragMonitor;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
     @FXML
-    private TextField username;
+    private JFXTextField username;
     @FXML
-    private Button login;
+    private JFXPasswordField password;
+
+    private boolean isRegister = false;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -21,7 +26,22 @@ public class LoginController implements Initializable {
     }
 
     @FXML
-    public void exit(){
+    public void register(MouseEvent e) {
+        if (isRegister) {
+            return;
+        }
+        isRegister = true;
+        HBox obj = (HBox) e.getSource();
+        WindowDragMonitor.register(obj.getScene().getWindow(), obj);
+    }
+
+    @FXML
+    public void login() {
+        System.out.println("---------");
+    }
+
+    @FXML
+    public void exit() {
         Platform.exit();
     }
 }
