9d8349c62ad30671ae9ba6df6cbf0f7157152870
navigatorCode <aaa123456>
2019-09-03 21:47:47 +0800

    去除mall-admin-client模块

--- mall-admin-client/src/main/java/cn/navigational/admin/client/config/Constants.java
+++
@@ -1,7 +0,0 @@
-package cn.navigational.admin.client.config;
-
-public class Constants {
-    public static final String ROOT = "root";
-    public static final String TITLE = "title";
-    public static final String CHILDREN = "children";
-}
--- mall-admin-client/src/main/java/cn/navigational/admin/client/routers/NotFoundRouter.java
+++
@@ -1,7 +0,0 @@
-package cn.navigational.admin.client.routers;
-
-import cn.navigational.admin.client.base.BaseRouteContent;
-
-public class NotFoundRouter extends BaseRouteContent {
-
-}
