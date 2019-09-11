7f48968117395b32666271458cdfed8828ae098b
navigatorCode <aaa123456>
2019-09-11 20:49:16 +0800

    修复unit test执行报错问题

--- mall-admin/src/test/java/cn/navigational/BaseTest.java
+++ mall-admin/src/test/java/cn/navigational/BaseTest.java
@@ -13,6 +13,7 @@ import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 
@@ -25,6 +26,7 @@ public class BaseTest {
     protected final static Vertx vertx = Vertx.vertx();
     protected String host = "localhost";
     protected int port = 8081;
+    protected JsonObject user;
 
 
     /**
@@ -34,14 +36,15 @@ public class BaseTest {
      */
     @Before
     public void start(TestContext context) {
+        Async async = context.async();
         String url ="/api/user/login";
         JsonObject info = new JsonObject();
         info.put("username","admin");
         info.put("password","123456");
         WebClient webClient = WebClient.create(vertx);
         webClient.post(port,host,url).sendJson(info,ar->{
-            JsonObject result = commonAsset(ar,context);
-            context.async().complete();
+            user = commonAsset(ar,context);
+            async.complete();
         });
 
     }
@@ -54,7 +57,7 @@ public class BaseTest {
         return options;
     }
 
-    private JsonObject commonAsset(AsyncResult<HttpResponse<Buffer>> ar, TestContext context){
+    protected JsonObject commonAsset(AsyncResult<HttpResponse<Buffer>> ar, TestContext context){
         if (ar.failed()){
             context.fail(ar.cause());
             return null;
@@ -66,8 +69,8 @@ public class BaseTest {
         return obj;
     }
 
-    @After
-    public void stop(TestContext context) {
+    @AfterClass
+    public static void stop(TestContext context) {
         vertx.close(context.asyncAssertSuccess());
     }
 }
--- mall-admin/src/test/java/cn/navigational/impl/UserTest.java
+++ mall-admin/src/test/java/cn/navigational/impl/UserTest.java
@@ -4,9 +4,17 @@ import cn.navigational.BaseTest;
 import cn.navigational.api.ApiVerticle;
 import cn.navigational.routers.UserRouter;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.WebClient;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static cn.navigational.config.Constants.DATA;
+import static cn.navigational.config.Constants.TOKEN;
 
 public class UserTest extends BaseTest {
 
@@ -18,7 +26,15 @@ public class UserTest extends BaseTest {
     }
 
     @Test
-    public void testLogin(TestContext context) {
-        context.async().complete();
+    public void testUserPermission(TestContext context) {
+        Async async = context.async();
+        String url = "/api/user/userPermission";
+        WebClient client = WebClient.create(vertx);
+        HttpRequest<Buffer> request = client.get(port, host, url);
+        request.headers().add("Authorization", "Bearer "+user.getJsonObject(DATA).getString(TOKEN));
+        request.send(ar -> {
+            commonAsset(ar, context);
+            async.complete();
+        });
     }
 }
